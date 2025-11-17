aabc1184bdb31dbaa1a24d7930a844ce44f3a8fa
Julien THOMAS <thomas.julien@protonmail.com>
2025-11-17 17:41:12 +0100

    Add peek and into_iter

--- src/bin/ok_stack.rs
+++ src/bin/ok_stack.rs
@@ -1,9 +1,9 @@
+type Link<T> = Option<Box<Node<T>>>;
+
 #[derive(Debug)]
 struct List<T> {
     head: Link<T>,
 }
-
-type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug)]
 struct Node<T> {
@@ -11,9 +11,15 @@ struct Node<T> {
     next: Link<T>,
 }
 
+struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     const fn new() -> Self {
         Self { head: Option::None }
+    }
+
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 
     fn push(&mut self, v: T) {
@@ -29,6 +35,14 @@ impl<T> List<T> {
             node.value
         })
     }
+
+    fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|e| &e.value)
+    }
+
+    fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|e| &mut e.value)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -40,13 +54,25 @@ impl<T> Drop for List<T> {
     }
 }
 
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 fn main() {
     let mut list = List::new();
     list.push(1u16);
     list.push(2);
     list.push(3);
     list.push(4);
-    println!("poping: {:?}", list.pop());
+
+    println!("peeking: {:?}", list.peek());
+
+    *list.peek_mut().unwrap() = 5;
+
     println!("poping: {:?}", list.pop());
     println!("poping: {:?}", list.pop());
     println!("poping: {:?}", list.pop());
