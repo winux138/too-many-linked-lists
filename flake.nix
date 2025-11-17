d3094066a2bc874e75f65cc966810a3f9f6cbaf0
Julien THOMAS <thomas.julien@protonmail.com>
2025-11-17 10:47:31 +0100

    Init rust project

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "{{NAME}}";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      flake-utils,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            ];
+
+            shellHook = '''';
+          };
+      }
+    );
+}
